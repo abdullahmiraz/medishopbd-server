e77f511145d99fd84a95903d5bebd5801814e06c
abdullahmiraz <abdullahmiraz2015@gmail.com>
2024-06-20 23:03:29 +0600

    little changed server.js, userController.js

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// app.use(cors());
-// app.use(express.json()); // This is crucial for parsing JSON bodies
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -13,11 +8,3 @@ app.listen(PORT, () => {
 app.use("/", (req, res) => {
   res.send("Server is running");
 });
-
-// app.use((req, res, next) => {
-//   console.log(
-//     `Received ${req.method} request on ${req.url} with body:`,
-//     req.body
-//   );
-//   next();
-// });
